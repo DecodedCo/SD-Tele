07987e8eae5168d4cbdb0d6cf29268bb33f150a0
Alex <alex@decoded.co>
2015-12-14 12:15:57 +0000

    latest keys

--- sendSMS.py
+++ sendSMS.py
@@ -1,15 +1,15 @@
 from twilio.rest import TwilioRestClient 
  
 # temporary SID
-ACCOUNT_SID = "SK4b09f7c9387af3cd29a7632c5abb92ab" 
+ACCOUNT_SID = "ACbec6453793a6556106fb9b1458b8a1fb" 
 #temporary secret
-AUTH_TOKEN = "0GpMTfrpozAMho56LByV5QAcQKPi" #missing characters 
+AUTH_TOKEN = "1797babb76a8f68db5c40608f11" #missing characters 
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
  #set the details here
 client.messages.create(
-	to="+44[to address]", 
-	from_="[from name", 
-	body="[message content]",  
+	to="+447725698210", 
+	from_="Alex", 
+	body="testing",  
 )
