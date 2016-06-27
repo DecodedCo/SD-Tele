d76c442d161640e559f13c149cbe31b78ac34980
Amadeus <github@amadeus.me.uk>
2016-06-27 11:32:47 +0200

    Update comments

--- sendSMS.py
+++ sendSMS.py
@@ -1,15 +1,15 @@
 from twilio.rest import TwilioRestClient 
  
-# temporary SID
+# Twilio SID
 ACCOUNT_SID = "ACbec6453793a6556106fb9b1458b8a1fb" 
-#temporary secret
-AUTH_TOKEN = "1797babb76a8f68db5c40608f11" #missing characters 
+# Twilio Primary Token (replace)
+AUTH_TOKEN = "XXXXX"
  
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
- #set the details here
+#set the details here
 client.messages.create(
 	to="+44[target phone number]", 
-	from_="[from text]", 
+	from_="[from text]", # 11 char max for short codes
 	body="[body of message]",  
 )
